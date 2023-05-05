c87a9a6721b823be31811e6fdda6bf77f05d5142
KATI LEE <katherineleefoley@gmail.com>
2023-05-05 16:25:44 +0000

    i dont think i acp-ed last night when i was done so just want to make sure everything gets saved

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 'use strict';
+console.log('sup nerds?')
 
 const express = require ('express');
 require ('dotenv').config();
-// let data = require();
+let data = require('./data/weather.json');
 const cors = require('cors');
-const { response } = require('express');
-
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5005;
 
-app.get('/', (request, response) => {
-    response.send('this is the server home route / !');
+app.get('/', (req, res) => {
+    response.send('this is the server');
 });
 
+app.get('/weather', async (req, res, next) => {
+
+    try {
+      let searchQuery = request.query.searchQuery;
+      let weatherDataObject = data.find(ele => ele.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase());
+      let dataTosend = weatherDataObject.data.map(forecast => new Forecast(forecast));
+      console.log(dataTosend, 'datatosend');
+      response.status(200).send(dataTosend);
+    } catch (error) {
+      next (error);
+    }
+  });
+
+app.get('*', (req, res) => {
+    response.status(404).send('No route was found, error 404.');
+});
+
+app.use((error, req, res) => {
+    response.status(500).send(error.message);
+});
+
+class Forecast{
+    constructor(forecastObject) {
+      this.date = forecastObject.datetime;
+      this.description = forecastObject.weather.description;
+    }
+  }
+
+app.listen(PORT, () => console.log (`listening on port ${PORT}`));
 
 // const Weather = require('');
 // const Movies = require('');
 // const Yelp = require('');
 
-// app.get('/weather', Weather.weatherRequest);
 // app.get('/movie', Movie.movieRequest);
 // app.get('/yelp', Yelp.yelpRequest);
-
-app.get('*', (request, response) => {
-    response.status(404).send('No route was found, error 404.');
-});
-
-app.use((error, request, response) => {
-    response.status(500).send(error.message);
-});
-
-
-app.listen(PORT, () => console.log (`listening on port ${PORT}`));
