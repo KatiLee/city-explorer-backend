e2aae22c845be9decd9987e103f2047fed08383e
KATI LEE <katherineleefoley@gmail.com>
2023-05-05 18:13:45 +0000

    im going to try working backwards so going to modularize it, its broke now but i just finished the server

--- server.js
+++ server.js
@@ -2,50 +2,29 @@
 console.log('sup nerds?')
 
 const express = require ('express');
-require ('dotenv').config();
-let data = require('./data/weather.json');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 app.use(cors());
+
+let data = require('./data/weather.json');
+const Weather = require('./modules/weather');
+const Movie = require('./modules/movie');
+// const Yelp = require('./modules/yelp');
+
 const PORT = process.env.PORT || 5005;
 
-app.get('/', (req, res) => {
-    response.send('this is the server');
-});
+app.get('/', (req, res) => response.send('this is the server')
+);
 
-app.get('/weather', async (req, res, next) => {
+app.get('/weather', Weather.weatherReq);
+app.get('/movie', Movie.movieReq);
+// app.get('/yelp', Yelp.yelpReq);
 
-    try {
-      let searchQuery = request.query.searchQuery;
-      let weatherDataObject = data.find(ele => ele.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase());
-      let dataTosend = weatherDataObject.data.map(forecast => new Forecast(forecast));
-      console.log(dataTosend, 'datatosend');
-      response.status(200).send(dataTosend);
-    } catch (error) {
-      next (error);
-    }
-  });
+app.get('*', (req, res) => response.status(404).send('No route was found, error 404.')
+);
 
-app.get('*', (req, res) => {
-    response.status(404).send('No route was found, error 404.');
-});
-
-app.use((error, req, res) => {
-    response.status(500).send(error.message);
-});
-
-class Forecast{
-    constructor(forecastObject) {
-      this.date = forecastObject.datetime;
-      this.description = forecastObject.weather.description;
-    }
-  }
+app.use((error, req, res) => response.status(500).send(error.message)
+);
 
 app.listen(PORT, () => console.log (`listening on port ${PORT}`));
-
-// const Weather = require('');
-// const Movies = require('');
-// const Yelp = require('');
-
-// app.get('/movie', Movie.movieRequest);
-// app.get('/yelp', Yelp.yelpRequest);
